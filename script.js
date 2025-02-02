336e7227685e9392caac5bcf0d7bb3c022d4b381
Pelhate Lenny <lenny.pelhate@student-cs.fr>
2025-02-02 01:02:59 +0100

    update paragraph

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.addEventListener('scroll', function() {
 // TYPED
 var typed = new Typed('.typed', {
     // Waits 1000ms after typing "First"
-    strings: ["^1200 Hi! I am a French student currently doing a double master's degree at ESSEC Business School and CentraleSupélec. ^1500 Passionate about finance, I am keen on learning more about this field and enthusiastic about the idea of developing new skills in order to pursue a future career in market finance. ^2000 I consider myself dynamic, perseverant and hard-working. ^700 I like to think ahead and beyond regular solutions for ordinary problems. ^1000 I enjoy new challenges and the idea of having to solve complex issues under high pressure, as they push me to excel and better myself. ^1000 Finally yet importantly, I cherish the values of teamwork and organization to accomplish tasks in time."],//à chaque virgule il repart en arrière
+    strings: ["^1200 Hi! I am a French student currently doing a double master's degree at ESSEC Business School and CentraleSupélec. ^1500 Passionate about finance, I am keen on learning more about this field and enthusiastic about the idea of developing new skills in order to pursue a future career in market finance. ^2000 I consider myself dynamic, perseverant and hard-working. ^700 I like to think ahead and beyond regular solutions for ordinary problems. ^1000 I enjoy new challenges and the idea of having to solve complex issues under high pressure, as they push me to excel and better myself. ^1000 Last but not least, I cherish the values of teamwork and organization to accomplish tasks in time."],//à chaque virgule il repart en arrière
     typeSpeed: 10,
     backSpeed: 0,
     loop: false
